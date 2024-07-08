0fd897b3b1f3e0211a9ccf8c01c21b6c76080594
RafsanBinAli <mr.chese.2@gmail.com>
2024-07-08 23:31:23 +0600

    console log some issues in protected route and passport

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 dotenv.config();
 require("./config/passport");
 
 // Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB })
-  .then(() => console.log('MongoDB Atlas connected'))
-  .catch(err => console.error('MongoDB Atlas connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, { dbName: process.env.DB })
+  .then(() => console.log("MongoDB Atlas connected"))
+  .catch((err) => console.error("MongoDB Atlas connection error:", err));
 
 const app = express();
-app.use(cookieParser());
-app.use(session({
-  secret: 'secret', 
-  resave: false, 
-  saveUninitialized: true,
-  cookie: {
-    secure: false,
-    httpOnly: false,
-    sameSite: 'none'
-  }
-}));
 app.use(cors());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
 const authRoutes = require("./routes/authRoute");
-const protectedRoute = require('./routes/protectedRoute');
+const protectedRoute = require("./routes/protectedRoute");
 
 app.use("/", authRoutes);
 app.use("/protected-route", protectedRoute);
